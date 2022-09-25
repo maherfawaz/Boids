944ca3fb8a941e1318f14a0180c0343f2bf632d5
maherfawaz <maherfawaz@outlook.com>
2022-09-24 20:01:28 -0400

    Added project and made build

---
+++ Assets/LookAtAttractor.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtAttractor : MonoBehaviour {
+    
+    void Update() {
+        transform.LookAt(Attractor.POS);
+    }
+}
